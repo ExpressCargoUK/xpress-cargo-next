3f3574344150bf02252bd2c9cd1d1f6edb80195f
Sachithra <sachithra.rash@gmail.com>
2023-07-12 10:48:35 +0530

    Fix: build errors

--- pages/_app.js
+++ pages/_app.js
@@ -91,7 +91,7 @@ export default function App({ Component, pageProps }) {
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
 
-        <Script strategy="lazyOnload">
+        <Script strategy="lazyOnload" id="googleAnalytics">
           {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
